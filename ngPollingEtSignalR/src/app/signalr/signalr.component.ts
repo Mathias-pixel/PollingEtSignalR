5df5de7d4294ac534a44e4ba15e0a5da6ea2e9cb
Mathias <2067934@cegepmontpetit.ca>
2024-09-13 11:12:10 -0400

    labo pa sfinit

--- ngPollingEtSignalR/src/app/signalr/signalr.component.ts
+++ ngPollingEtSignalR/src/app/signalr/signalr.component.ts
@@ -22,13 +22,28 @@ export class SignalrComponent implements OnInit {
   connecttohub() {
     // TODO On doit commencer par créer la connexion vers le Hub
 
+    this.hubConnection = new signalR.HubConnectionBuilder()
+    .withUrl('https://localhost:7060/chat')
+    .build();
+    
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
 
+    this.hubConnection!.on('UneFonction', (data) => {
+  
+      console.log(data);
+  });
     // TODO On doit ensuite se connecter
+    this.hubConnection
+        .start()
+        .then(() => {
+            console.log('La connexion est active!');
+          })
+        .catch(err => console.log('Error while starting connection: ' + err));
   }
 
   complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
+
   }
 
   addtask() {
